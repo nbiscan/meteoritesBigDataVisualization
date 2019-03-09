4c4556cb11c4e90022471f572b7892af3a25e674
Natko Biscan <nbiscan7@gmail.com>
2019-03-09 17:35:54 +0100

    Configure leaflet maps

--- frontend/meteorites/src/App.js
+++ frontend/meteorites/src/App.js
@@ -1,147 +1,168 @@
-import React, { Component } from 'react';
-import { VectorMap } from "react-jvectormap"
-import { Button } from 'react-bootstrap';
-import './App.css';
+import React, { Component } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import testMarkers from "./testMarkers";
+import { Button } from "react-bootstrap";
+import "./App.css";
 
-class App extends Component {
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
 
+L.Marker.prototype.options.icon = DefaultIcon;
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.ws = new WebSocket('ws://localhost:9000/ws');
+    // this.ws = new WebSocket("ws://localhost:9000/ws");
     this.state = {
-      serverMarkers: [],
+      zoom: 4,
+      serverMarkers: testMarkers,
       queryResult: [],
       markerStyle: {
         initial: {
-          fill: '#F8E23B',
-          stroke: '#383f47'
+          fill: "#F8E23B",
+          stroke: "#383f47"
         },
         hover: {
           "fill-opacity": 0.8,
-          cursor: 'pointer',
-          fill: 'red',
-          stroke: '#383f47'
-        },
+          cursor: "pointer",
+          fill: "red",
+          stroke: "#383f47"
+        }
       },
+      testMarkers
     };
-    this.testMarkers = [{ "fall": "Fell", "latLng": [36.23333, 37.13333], "id": "462", "mass": "3200", "name": "Aleppo", "nametype": "Valid", "recclass": "L6", "reclat": "36.233330", "reclong": "37.133330", "year": "1873-01-01T00:00:00.000" }
-      , { "fall": "Fell", "latLng": [44.88333, 8.75], "id": "463", "mass": "908", "name": "Alessandria", "nametype": "Valid", "recclass": "H5", "reclat": "44.883330", "reclong": "8.750000", "year": "1860-01-01T00:00:00.000" }];
   }
 
   componentDidMount() {
     var tmpMarkers = [];
 
     fetch(`http://localhost:19002/query/service`, {
-      method: 'POST',
-      body: `select name, geolocation, year, mass from meteorites.meteorites_ds;`,
-    }).then((res) => res.json())
-      .then((response) => {
+      method: "POST",
+      body: `select name, geolocation, year, mass from meteorites.meteorites_ds;`
+    })
+      .then(res => res.json())
+      .then(response => {
         this.setState({
           queryResult: response.results
         });
-        console.log(this.state.queryResult)
 
         response.results.forEach(meteorite => {
-          var metYear = meteorite.year ? meteorite.year.split('-')[0] : 'unknown';
-          var metMass = meteorite.mass ? `${meteorite.mass.split('-')[0]} g` : 'unknown';
+          var metYear = meteorite.year
+            ? `${meteorite.year.split("-")[0]}.`
+            : "unknown";
+          var metMass = meteorite.mass
+            ? `${meteorite.mass.split("-")[0]} g`
+            : "unknown";
           tmpMarkers.push({
-            "name": `Name: ${meteorite.name}, Year: ${metYear}., Mass: ${metMass}`,
-            "latLng": meteorite.geolocation ? [meteorite.geolocation.coordinates[1], meteorite.geolocation.coordinates[0]] : undefined,
+            name: meteorite.name,
+            year: metYear,
+            mass: metMass,
+            latLng: meteorite.geolocation
+              ? [
+                  meteorite.geolocation.coordinates[1],
+                  meteorite.geolocation.coordinates[0]
+                ]
+              : undefined
           });
         });
         this.setState({
           serverMarkers: tmpMarkers
         });
-
       });
-
   }
 
   click(text) {
     var tmpMarkers = [];
 
     fetch(`http://localhost:19002/query/service`, {
-      method: 'POST',
-      body: `select name, geolocation, year, mass from meteorites.meteorites_ds where name like "%${text}%";`,
-    }).then((res) => res.json())
-      .then((response) => {
+      method: "POST",
+      body: `select name, geolocation, year, mass from meteorites.meteorites_ds where name like "%${text}%";`
+    })
+      .then(res => res.json())
+      .then(response => {
         this.setState({
           queryResult: response.results
         });
-        console.log(this.state.queryResult)
 
         response.results.forEach(meteorite => {
-          var metYear = meteorite.year ? meteorite.year.split('-')[0] : 'unknown';
-          var metMass = meteorite.mass ? `${meteorite.mass.split('-')[0]} g` : 'unknown';
+          var metYear = meteorite.year
+            ? `${meteorite.year.split("-")[0]}.`
+            : "unknown";
+          var metMass = meteorite.mass
+            ? `${meteorite.mass.split("-")[0]} g`
+            : "unknown";
           tmpMarkers.push({
-            "name": `Name: ${meteorite.name}, Year: ${metYear}., Mass: ${metMass}`,
-            "latLng": meteorite.geolocation ? [meteorite.geolocation.coordinates[1], meteorite.geolocation.coordinates[0]] : undefined,
+            name: meteorite.name,
+            year: metYear,
+            mass: metMass,
+            latLng: meteorite.geolocation
+              ? [
+                  meteorite.geolocation.coordinates[1],
+                  meteorite.geolocation.coordinates[0]
+                ]
+              : undefined
           });
         });
         this.setState({
           serverMarkers: tmpMarkers
         });
-
       });
 
     if (text === "") {
-      document.getElementsByClassName('form')[0].value = '';
+      document.getElementsByClassName("form")[0].value = "";
     }
-
-  }
-
-  onMarkerSelect() {
-    console.log('a');
-  }
-
-  handleData(data) {
-    let result = JSON.parse(data);
-    console.log(result.movement);
   }
 
   render() {
+    console.log(this.state.serverMarkers);
     return (
       <div>
         <div className="background">
           <div className="input">
             <input
               className="form"
-              placeholder='Enter full name or segment (please include capitalization)'
-              ref={(input) => {
+              placeholder="Enter full name or segment (please include capitalization)"
+              ref={input => {
                 this.text = input;
               }}
               onKeyPress={event => {
                 if (event.key === "Enter") {
                   this.click(this.text.value);
                 }
-              }} />
-            <div className='btn'>
-              <Button
-                onClick={() => this.click(this.text.value)}
-              >
+              }}
+            />
+            <div className="btn">
+              <Button onClick={() => this.click(this.text.value)}>
                 Search
-                </Button>
+              </Button>
             </div>
             <Button onClick={() => this.click("")}>Show all</Button>
           </div>
         </div>
-        <div style={{ height: '95vh', width: '100%' }}>
-          <VectorMap map={'world_mill'}
-            backgroundColor="#383f47"
-            ref="map"
-            containerStyle={{
-              width: '100%',
-              height: '100%'
-            }}
-            containerClassName="map"
-            regionStyle={this.state.regionStyle}
-            markerStyle={this.state.markerStyle}
-            markers={this.state.serverMarkers}
-            onMarkerSelected={this.onMarkerSelect}
+        <Map center={this.state.serverMarkers[0].latLng} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        </div>
-      </div >
-    )
+          {this.state.serverMarkers.map(
+            (marker, i) =>
+              marker.latLng && (
+                <Marker key={i} position={marker.latLng}>
+                  <Popup>
+                    <p>Name: {marker.name}</p>
+                    <p>Year: {marker.year}</p>
+                    <p>Mass: {marker.mass}</p>
+                  </Popup>
+                </Marker>
+              )
+          )}
+        </Map>
+      </div>
+    );
   }
 }
-export default App;
---
+++ frontend/meteorites/src/testMarkers.js
@@ -0,0 +1,28 @@
+const testMarkers = [
+  {
+    fall: "Fell",
+    latLng: [36.23333, 37.13333],
+    id: "462",
+    mass: "3200",
+    name: "Aleppo",
+    nametype: "Valid",
+    recclass: "L6",
+    reclat: "36.233330",
+    reclong: "37.133330",
+    year: "1873-01-01T00:00:00.000"
+  },
+  {
+    fall: "Fell",
+    latLng: [44.88333, 8.75],
+    id: "463",
+    mass: "908",
+    name: "Alessandria",
+    nametype: "Valid",
+    recclass: "H5",
+    reclat: "44.883330",
+    reclong: "8.750000",
+    year: "1860-01-01T00:00:00.000"
+  }
+];
+
+export default testMarkers;
